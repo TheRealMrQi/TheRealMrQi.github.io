1f4faa2692bd0a3a49513cdc664310b58378ef66
TheRealMrQi <therealmrqi@gmail.com>
2025-03-30 09:33:31 +0200

    Test for mobile function

--- JS.js
+++ JS.js
@@ -19,6 +19,21 @@ function ResizeElements()
 	var h = screen.height * ratio;
 	logo.style.left =  0+ document.body.clientWidth / 2 - logo.offsetWidth / 2 + 'px';
 	apeLogo.style.left =  0+ document.body.clientWidth / 2 - apeLogo.offsetWidth / 2 + 'px';
+	testElement.innerHTML = detectMob();
 }
 
-
+function detectMob() {
+    const toMatch = [
+        /Android/i,
+        /webOS/i,
+        /iPhone/i,
+        /iPad/i,
+        /iPod/i,
+        /BlackBerry/i,
+        /Windows Phone/i
+    ];
+    
+    return toMatch.some((toMatchItem) => {
+        return navigator.userAgent.match(toMatchItem);
+    });
+}
